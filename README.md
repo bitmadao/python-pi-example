f6a097b3f7410af95ab06799273395f5d56f60e3
Dag <gundag@gmail.com>
2018-06-23 13:42:30 +0200

    a new commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # python-pi-example
 Following along to a Udemy course on Linux.
 edit 1
+
+Make a change
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def main(msg):
+    # Add another comment
     # Adding second comment
     # print a message
     print(msg)
